ec0dcad6eb0e0508b19d45be30662f3cf6f84ee2
Lawrence <lawrence-nowak@web.de>
2019-05-06 18:02:26 +0200

    slidebuttion first funcionality

--- scripts/javascript.js
+++ scripts/javascript.js
@@ -7,9 +7,23 @@
 //  - check which image is displayed
 //  - on click switch to the image thats has to be displayed
 //=====================
-function slideButton(){
-    var slideOne 
-    if (){
 
-    }
-}
+
+function slideButton1(){
+    var imageOne = document.getElementById ("imageOne").style.display = "none";
+};
+
+function slideButton2(){
+    var imageTwo = document.getElementById ("imageTwo").style.display = "none"; 
+};
+
+function slideButton3(){
+    var imageThree = document.getElementById ("imageThree").style.display = "none";     
+};
+
+
+function slideButton(){
+    for (var i=1; i <= 1; i++){
+        if ()
+    };
+};
