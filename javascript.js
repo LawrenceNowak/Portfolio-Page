4f26a5eef13b51d7bd5de3cd6e86b3a0a37d062b
LawrenceNowak <44798880+LawrenceNowak@users.noreply.github.com>
2019-05-06 13:12:57 +0200

    Add files via upload

---
+++ javascript.js
@@ -0,0 +1,15 @@
+//=====================
+// SLIDEBUTTON FUNCTION
+//=====================
+
+//=====================
+// requirements
+//  - check which image is displayed
+//  - on click switch to the image thats has to be displayed
+//=====================
+function slideButton(){
+    var slideOne 
+    if (){
+
+    }
+}
