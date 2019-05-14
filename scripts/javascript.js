0462a99ee38e8fb0a66c6bff462a776e0b9919c0
Lawrence <lawrence-nowak@web.de>
2019-05-14 09:49:39 +0200

    refactoring code

--- scripts/javascript.js
+++ scripts/javascript.js
@@ -9,21 +9,3 @@
 //=====================
 
 
-function slideButton1(){
-    var imageOne = document.getElementById ("imageOne").style.display = "none";
-};
-
-function slideButton2(){
-    var imageTwo = document.getElementById ("imageTwo").style.display = "none"; 
-};
-
-function slideButton3(){
-    var imageThree = document.getElementById ("imageThree").style.display = "none";     
-};
-
-
-function slideButton(){
-    for (var i=1; i <= 1; i++){
-        if ()
-    };
-};
